f1dff15523e84f56d3e7d0bea5628bbe87b6856a
Andre Luchesi <professor.andreluchesi@gmail.com>
2025-11-18 09:46:56 -0300

    tirando as configuraçoes do Heroku

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Driver para PostgreSQL
-        'NAME': 'pfi', # Nome do banco criado no PostgreSQL
-        'USER': 'postgres', # Usuário do PostgreSQL
-        'PASSWORD': 'postgres', # Senha do usuário
-        'HOST': 'localhost', # Ou IP/hostname do servidor do banco
-        'PORT': '5432', # Porta padrão do PostgreSQL
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Driver para PostgreSQL
+#         'NAME': 'pfi', # Nome do banco criado no PostgreSQL
+#         'USER': 'postgres', # Usuário do PostgreSQL
+#         'PASSWORD': 'postgres', # Senha do usuário
+#         'HOST': 'localhost', # Ou IP/hostname do servidor do banco
+#         'PORT': '5432', # Porta padrão do PostgreSQL
+#     }
+# }
 
 
 # Password validation
@@ -144,4 +144,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #configuração Heroku
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
